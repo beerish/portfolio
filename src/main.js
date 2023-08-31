7bb2ff03ad86707ccad47a8215fe36cd3f68b0cf
beerish <plz.beerish@gmail.com>
2023-08-31 15:09:41 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -29,3 +29,15 @@ document.addEventListener('scroll', () => {
         arrow.classList.remove('arrow-up--invisible');
     }
 });
+
+//navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+//navbar 메뉴 클릭시 메뉴 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
+});
